6ebbdc8bd3970e35369749c8a6aa32ccdee155bd
Mikhail Prokopchuk <46092273+mishpro@users.noreply.github.com>
2022-10-30 00:42:15 +0300

    init - tuple problem

---
+++ 1_ha/b_problem.py
@@ -0,0 +1,19 @@
+def selection_sort(x):
+    N = len(x)
+    for i in range(N - 1):
+        i_min = i
+        for j in range(i + 1, N):
+            if x[i_min] > x[j]:
+                i_min = j
+        x[i], x[i_min] = x[i_min], x[i]
+
+
+if __name__ == '__main__':
+    N = int(input())
+    x = []
+
+    for i in range(N):
+        id, name, score = input().split()
+        x.append(tuple(-score, id, name))
+
+    print(x)
