cdc143d285047e31487f6aa65f8cdcf353857398
MikhailProkopchuk <mike.prokopski@gmail.com>
2022-11-25 21:38:13 +0300

    raw treasure list collected

---
+++ 1_ha/d_problem.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    N, W = map(int, input().split())
+    raw_tres = []
+    for i in range(N):
+        n, w = map(int, input().split())
+        raw_tres.append((n, w))
